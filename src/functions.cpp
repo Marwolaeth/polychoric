ec57d9e2c5489ca733370d40c999f914b61626e3
Culturology's Undead <a.pawluczenko@gmail.com>
2023-06-06 23:11:20 +0300

    No to parallel

--- src/functions.cpp
+++ src/functions.cpp
@@ -15,8 +15,6 @@
 #include "toms462.cpp"
 #include "LBFGSB.h"
 
-Eigen::setNbThreads(Eigen::nbThreads() - 1);
-
 /*
  TOMS462 is a C++ library which evaluates the upper right tail of the bivariate
  normal distribution; that is, the probability that normal variables
